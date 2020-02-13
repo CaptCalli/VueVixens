ad120371d174493f9eb24325395b0ab1a545a6ca
Caroline Kerns <48072006+CaptCalli@users.noreply.github.com>
2020-02-13 10:54:35 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,179 @@
+const HEROINES = [
+	{ 	Img: { src: "img/alispittel.png", alt: "Ali Spittel Image"},
+		Color: { background: "#8ADC7B", text: "black" },
+		Name: "Ali Spittel",
+		Job: "Software Engineer",
+		Description: "Ali is a software engineer at DEV. Before that, she was a lead instructor at General Assembly. She loves Python, JavaScript, and talking about code. She is most interested in the intersection of code, art, and social justice. </p> <p> When Ali's not working, you can find her watching New England sports, competing on CodeWars, taking runs around Capitol Hill, rock climbing, or participating in DC coding community events. </p> <p> Ali also blogs at <a href='http://www.dev.to/aspittel'>dev.to/aspittel</a> where she talks about code and her life surrounding it.",
+		Twitter: { link: "https://twitter.com/aspittel", name: "@ASpittel" }
+	},
+	{
+		Img: { src: "img/alyssanicoll.png", alt: "Alyssa Nicoll Image"},
+		Color: { background: "#F2DF43", text: "black"},
+		Name: "Alyssa Nicoll",
+		Job: "Developer Advocate",
+		Description: "Alyssa is an Angular Developer Advocate and Google Developer Expert. Her two degrees in Web Design and Development and Psychology feed her speaking career. She has spoken at over 30 conferences internationally. </p> <p> Alyssa is a weekly panelist on Adventures in Angular and Angular Air, which have a combined following of over 16,000 listeners. She enjoys gaming, scuba diving, and has a furry wiener dog named Beef.",
+		Twitter: { link: "https://twitter.com/alyssanicoll", name: "@AlyssaNicoll" }
+	},
+	{
+		Img: { src: "img/angelbanks.png", alt: "Angel Banks Image"},
+		Color: { background: "#F64C61", text: "black"},
+		Name: "Angel Banks",
+		Job: "Software Developer",
+		Description: "Angel Banks has a background in front-end development and currently specializes in product management and design strategy for enterprise, finance, healthcare, and biotech applications. </p> <p> Active in the Atlanta tech community, she is a Director of Women Who Code Atlanta, Women Techmakers Atlanta Lead, GDG Atlanta Organizer, and is a conference organizer for REFACTR.TECH and DevFest Atlanta.",
+		Twitter: { link: "https://twitter.com/angelmbanks", name: "@angelmbanks" }
+	},
+	{
+		Img: { src: "img/anjanavakil2.png", alt: "Anjana Vakil Image"},
+		Color: { background: "#FF582D", text: "white"},
+		Name: "Anjana Vakil",
+		Job: "Software Developer",
+		Description: "Anjana suffers from a debilitating case of curiosity, which led her from philosophy to English teaching to computational linguistics to software development </p> <p> A Mozilla TechSpeaker and alumna of the Recurse Center and Outreachy, she can often be found coding in San Francisco; that is, when she's not speaking at events around the world to share the joy of programming and promote a more diverse and inclusive tech industry. </p> <p> Talk to her about programming paradigms, language design, and linguistics.",
+		Twitter: { link: "https://twitter.com/anjanavakil", name: "@AnjanaVakil" }
+	},
+	{
+		Img: { src: "img/aysegulyonet.png", alt: "Aysegul Yonet Image"},
+		Color: { background: "#EF0D33", text: "white"},
+		Name: "Ay&#351;eg&uuml;l Y&ouml;net",
+		Job: "Software Engineer",
+		Description: "Ay&#351;eg&uuml;l is a Software Engineer who focuses on Data Visualization and Mixed Reality. She is a Google Developer Expert for Web Technologies and Angular and is an active conference speaker. </p> <p> She enjoys teaching at a variety of non-profit organizations aiming to increase diversity in the software industry. She is the co-founder of code4good, an organization with the goal of helping people to contribute to open source.",
+		Twitter: { link: "https://twitter.com/ayssomething", name: "@AysSomething" }
+	},
+	{
+		Img: { src: "img/bethlaing.png", alt: "Beth Laing Image"},
+		Color: { background: "#A33A49", text: "white"},
+		Name: "Beth Laing",
+		Job: "Software Developer",
+		Description: "Beth grew up in online journalism, working with newspaper companies before taking a detour to work for an internet startup, Infonautics. She transitioned into software development with the help of a coding bootcamp. </p> <p> As one of the directors for Women Who Code, Atlanta, Beth works to create great programming and build community for the amazing women in technology of Atlanta. Beth was a software developer at CNN/Turner and in March 2019 joined CallRail as an Engineering Manager.",
+		Twitter: { link: "https://twitter.com/laingbeth", name: "@laingbeth" }
+	},
+	{
+		Img: { src: "img/dianarodriguez2.png", alt: "Diana Rodriguez Image"},
+		Color: { background: "#B34ACB", text: "white"},
+		Name: "Diana Rodriguez",
+		Job: "Chief DevOps Architect",
+		Description: "Diana Rodriguez is a Full Stack Developer/DevOps and Google Developer Expert. With 18 years' experience and a strong background in back end and infrastructure, Diana likes to bring together the best of both worlds, spreading DevOps culture. </p> <p> She's super enthusiastic about everything to encourage people to start a career in development and a fan of female devs and DevOps. In her spare time she likes to play videogames, also a huge fan of sports, Diana is a rugby player and a taekwondo black belt.",
+		Twitter: { link: "https://twitter.com/cotufa82", name: "@cotufa82" }
+	},
+	{
+		Img: { src: "img/divyasasidharan.png", alt: "Divya Sasidharan Image"},
+		Color: { background: "#F0A036", text: "black"},
+		Name: "Divya Sasidharan",
+		Job: "Developer Advocate",
+		Description: "Divya is a developer advocate at Netlify where she champions the JAMstack and works on making frontend developers more powerful to optimize the developer experience. </p> <p> You will most likely find her in the sunniest spot in the room with a cup of tea and perhaps a book in hand.",
+		Twitter: { link: "https://twitter.com/shortdiv", name: "@shortdiv" }
+	},
+	{
+		Img: { src: "img/emmabostian.png", alt: "Emma Bostian Image"},
+		Color: { background: "#E8114A", text: "white"},
+		Name: "Emma Bostian",
+		Job: "UX Engineer",
+		Description: "Emma is a Software Engineer turned UX Engineer. She worked for 3 years as a Software Engineer at IBM in Austin, Texas. Currently, Emma works at LogMeIn in Karlsruhe, Germany building and teaching Design Systems. </p> <p> In her free time, Emma enjoys blogging, reading books, and traveling. She's also building Coding Coach and creating courses on <a href='http://Egghead.io'>Egghead.io</a>.",
+		Twitter: { link: "https://twitter.com/emmabostian", name: "@EmmaBostian" }
+	},	
+	{
+		Img: { src: "img/jenlooper2.png", alt: "Jen Looper Image"},
+		Color: { background: "#C4D8D0", text: "black"},
+		Name: "Jen Looper",
+		Job: "Developer Advocate",
+		Description: "Jen Looper  is a Google Developer Expert and a Senior Developer Advocate with over 18 years' experience as a web and mobile developer, specializing in creating cross-platform mobile apps. </p> <p> She's a multilingual multiculturalist with a passion for hardware hacking, mobile apps, Vue.js, machine learning and discovering new things every day. </p> <p> She is the founder and CEO of Vue Vixens, an initiative promoting diversity in the Vue.js community.",
+		Twitter: { link: "https://twitter.com/jenlooper", name: "@jenlooper" }
+	},
+	{
+		Img: { src: "img/jofranchetti.png", alt: "Jo Franchetti Image"},
+		Color: { background: "#FFFD90", text: "black"},
+		Name: "Jo Franchetti",
+		Job: "Developer Advocate",
+		Description: "Jo is a Web Developer Advocate for Samsung internet who is passionate about WebVR, PWAs and beautiful CSS. She is a front end developer who has worked in various parts of the tech industry from startups, agencies, and charities to large organizations. </p> <p> Jo is also a mentor and organiser at codebar.io where she is able to action her passion for teaching good use of the web and for improving the diversity and inclusivity of the tech industry.",
+		Twitter: { link: "https://twitter.com/thisisjofrank", name: "@ThisIsJoFrank" }
+	},
+	{
+		Img: { src: "img/karaerickson2.png", alt: "Kara Erickson Image"},
+		Color: { background: "#A5D933", text: "black"},
+		Name: "Kara Erickson",
+		Job: "Software Engineer",
+		Description: "Kara is the tech lead for the Angular framework team at Google. These days, she spends most of her time working on the next generation of the Angular rendering engine (project Ivy). </p> <p> Prior to Google, she helped build UI components in Angular at OpenTable. She enjoys snacking indiscriminately and reading fantasy books, among other things.",
+		Twitter: { link: "https://twitter.com/karaforthewin", name: "@karaforthewin" }
+	},	
+	{
+		Img: { src: "img/nataliatepluhina.png", alt: "Natalia Tepluhina Image"},
+		Color: { background: "#7B46D9", text: "white"},
+		Name: "Natalia Tepluhina",
+		Job: "Senior Frontend Developer",
+		Description: "Natalia Tepluhina is a frontend developer, a big fan of Vue.js framework and a contributor to the Vue ecosystem. She works as a Senior Frontend Engineer at GitLab. </p> <p> Natalia is a constant learner and loves to share knowledge with other people. She is a conference speaker and author of articles on different topics related to Vue.js. Thanks to these activities, she is a Google Developer Expert in Web Technologies. </p> <p> Natalia is also CTO of Vue Vixens, the initiative that creates and hosts workshops to teach Vue.js to women in a cool and fun way.",
+		Twitter: { link: "https://twitter.com/n_tepluhina", name: "@n_tepluhina" }
+	},
+	{
+		Img: { src: "img/nityanarasimhan.png", alt: "Nitya Narasimhan Image"},
+		Color: { background: "#EF1731", text: "white"},
+		Name: "Nitya Narasimhan",
+		Job: "Developer Advocate",
+		Description: "Nitya is a PhD and Polyglot with 20+ years of software research & development across industry, startups and academia. </p> <p> She is a well-respected teach speaker, conference and community organizer, passionate about helping others translate technology awareness into actionable impact on product and personal growth.",
+		Twitter: { link: "https://twitter.com/nitya", name: "@nitya" }
+	},
+	{
+		Img: { src: "img/sarahdrasner.png", alt: "Sarah Drasner Image"},
+		Color: { background: "#00BBC7", text: "black"},
+		Name: "Sarah Drasner",
+		Job: "Principal Developer Advocate",
+		Description: "Sarah Drasner is an award-winning Speaker, Lead of Emerging Markets for Cloud Advocates at Microsoft, and Staff Writer at CSS-Tricks. Sarah is also the co-founder of Web Animation Workshops with Val Head and is a member of the Vue Core Team. </p> <p> Sarah is also the author of the SVG animations book from O'Reilly and was formerly a Manager of UX and Engineering at Trulia (Zillow).",
+		Twitter: { link: "https://twitter.com/sarah_edo", name: "@sarah_edo" }
+	},
+	{
+		Img: { src: "img/saravieira.png", alt: "Sara Vieira Image"},
+		Color: { background: "#48A261", text: "black"},
+		Name: "Sara Vieira",
+		Job: "Developer",
+		Description: "Sara is a developer at CodeSandbox.io. She's a big GraphQL and Open Source enthusiast. </p> <p> She is also a well-known conference speaker and describes herself as an airport expert. She is also a drummer and is into horror movies.",
+		Twitter: { link: "https://twitter.com/NikkitaFTW", name: "@NikkitaFTW" }
+	},
+	{
+		Img: { src: "img/simonacotin.png", alt: "Simona Cotin Image"},
+		Color: { background: "#868CB8", text: "black"},
+		Name: "Simona Cotin",
+		Job: "Developer Advocate",
+		Description: "Simona Cotin is a web developer with a passion for teaching. She spends most of her time tinkering with JavaScript in the cloud and sharing her epxerience with other developers at community events. </p> <p> As a Cloud Developer Advocate, Simona engages with the web community to help create a great developer experience with Azure. She loves shipping code to production and has built network data analytics platforms using Angular, TypeScript, React, and Node.js.",
+		Twitter: { link: "https://twitter.com/simona_cotin", name: "@simona_cotin" }
+	},
+	{
+		Img: { src: "img/stanimiravlaeva.png", alt: "Stanimira Vlaeva Image"},
+		Color: { background: "#5299FF", text: "black"},
+		Name: "Stanimira Vlaeva",
+		Job: "Software Engineer",
+		Description: "Stanimira is a software engineer at Progress and is a NativeScript core team. She loves everything about Angular and is Webpack's most passionate supporter on the NativeScript team. </p> <p> Her superpowers include debugging errors late at night and obsessing over old movies and gigs.",
+		Twitter: { link: "https://twitter.com/stanimiravlaeva", name: "@StanimiraVlaeva" }
+	},
+	{
+		Img: { src: "img/suzhinton.png", alt: "Suz Hinton Image"},
+		Color: { background: "#81DFAE", text: "black"},
+		Name: "Suz Hinton",
+		Job: "Developer Advocate",
+		Description: "Suz specializes in IoT, cloud computing, and web accessibility. She maintains many open source JavaScript hardware related libraries, some of which you might have used if you've tinkered with an Arduino board before. </p> <p> Suz likes dreaming up fun electronic projects in her spare time and bringing them to life.",
+		Twitter: { link: "https://twitter.com/noopkat", name: "@noopkat" }
+	},
+	{
+		Img: { src: "img/taramanicsic.png", alt: "Tara Z. Manicsic Image"},
+		Color: { background: "#00A4C1", text: "white"},
+		Name: "Tara Z. Manicsic",
+		Job: "Developer Advocate",
+		Description: "Tara Z. Manicsic is a lifelong student, teacher, and maker. She has spent her career using JavaScript on both the back-end and front-end to create applications. A Developer Advocate for Progress, Google Developer Expert, and international technical speaker, she focuses on conveying the codebase she has learned. </p> <p> In her free time she works in her community to educate and learn from other developers. Tara launched the Cincinnati Chapter of Women Who Code and the Cincinnati branch of NodeSchool. Beyond code, Tara still makes things but with solder, clay, or wool and hikes any mountain she can get to with her loves: Matthew, Niko & Toshmagosh.",
+		Twitter: { link: "https://twitter.com/tzmanics", name: "@Tzmanics" }
+	}
+];
+
+for (const heroine of HEROINES){
+	const div = `<div class="flip-card five wide column">` +
+  				`	<div class="flip-card-inner" style="background-color:${heroine.Color.background};color:${heroine.Color.text}">` +
+	    		`		<div class="flip-card-front">` +
+	      		`			<img src="${heroine.Img.src}" alt="${heroine.Img.alt}" class="ui centered medium image">` +
+	      		`			<h1>${heroine.Name}</h1>` +
+	      		`			<p class="jobs">${heroine.Job}</p>` +
+	    		`		</div>` +
+	    		`		<div class="flip-card-back">` +
+	      		`			<h1>${heroine.Name}</h1>` +
+	     		`			<p> <i class="twitter icon"></i> <a href="${heroine.Twitter.link}"> ${heroine.Twitter.name}</a>` +
+	      		`			<p> ${heroine.Description}</p>` +
+	    		`		</div>` +
+  				`	</div>` +
+				`</div>`;
+	$( ".root" ).append( div );
+}
